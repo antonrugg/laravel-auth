822f30440fef296750b4d0cb9ba51d8b85049a2d
Antonio Ruggiero <antonrugg@gmail.com>
2022-05-30 16:52:52 +0200

    deleted homecontroller default - added homcontroller admin - added route middleware for admin routes and uri

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+    });
+//il middleware e' il sfotware intermedio che quando chiamiamo una determinata rotta
+//ci passa attraverso e controlla l'autenticazione
+//in questo caso tutti i controller che hanno Admin nel namespace
+//prende tutte le rotte con il prefisso admin(gli uri della rotta) e il nome della rotta (route:list come riferimento)
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
